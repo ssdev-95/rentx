6340a819f6b4a21ab6449dd30b2436c0673bfd1c
Salomao S <souza95salomao@gmail.com>
2022-10-15 21:07:32 +0000

    feat: list all available cars

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 import { CreateCarController } from './controllers/cars/create'
+import { RetrieveCarsController } from './controllers/cars/retrieve'
 
 const router = Router()
 
 router.post(
 	'/cars/seed',
 	new CreateCarController().handle
+)
+
+router.get(
+	'/cars',
+	new RetrieveCarsController().handle
 )
 
 export { router }
