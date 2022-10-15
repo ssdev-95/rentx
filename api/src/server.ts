81de6861ecda2ddee4823243c18b2014085953b3
Salomao S <souza95salomao@gmail.com>
2022-10-15 22:47:24 +0000

    update: some dependencies

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,13 @@
+import cors from 'cors'
 import express from 'express'
 import { router } from './routes'
 
 const app = express()
 
+app.use(cors({
+	origin: '*'
+}))
+app.use(express.json())
 app.use(router)
 
 export { app }
