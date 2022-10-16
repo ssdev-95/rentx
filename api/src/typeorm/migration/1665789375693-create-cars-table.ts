3f8f5adfe3f23844fa56b3f16a2c41d8866b8562
Salomao S <souza95salomao@gmail.com>
2022-10-16 17:32:53 +0000

    update: orm settings and db migrations

--- api/src/typeorm/migration/1665789375693-create-cars-table.ts
+++ api/src/typeorm/migration/1665789375693-create-cars-table.ts
@@ -1,87 +1,82 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createCarsTable1665789375693 implements MigrationInterface {
-	private table = 'cars'
+ private table = 'cars'
 
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.createTable(new Table({
-			name: this.table,
-			columns: [
-				{
-					name: 'id',
-					type: 'varchar',
-					isNullable: false,
-					isUnique: true,
-					isPrimary: true,
-					isGenerated: true,
-					generationStrategy: 'uuid'
-				},
-				{
-					name: 'manufacturer',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'model',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name:'thumbs',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'rentPrice',
-					type: 'integer',
-					isNullable: false
-				},
-				{
-					name: 'description',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'maxPassengers',
-					type: 'integer',
-					isNullable: false
-				},
-				{
-					name: 'maxSpeed',
-					type: 'integer',
-					isNullable: false
-				},
-				{
-					name: 'fuelType',
-					type: 'varchar'
-				},
-				{
-					name:'cambioMode',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'horsePower',
-					type: 'integer',
-					isNullable: false
-				},
-				{
-					name: 'igniteTime',
-					type: 'integer',
-					isNullable: false
-				},
-				{
-					name: 'rents',
-					type: 'varchar',
-					isNullable: true,
-					default: null
-				}
-			]
-		}))
-	}
+ public async up(queryRunner: QueryRunner): Promise<void> {
+  await queryRunner.createTable(
+   new Table({
+    name: this.table,
+    columns: [
+     {
+      name: 'id',
+      type: 'varchar',
+      isNullable: false,
+      isUnique: true,
+      isPrimary: true,
+      isGenerated: true,
+      generationStrategy: 'uuid',
+     },
+     {
+      name: 'manufacturer',
+      type: 'varchar',
+      isNullable: false,
+     },
+     {
+      name: 'model',
+      type: 'varchar',
+      isNullable: false,
+     },
+     {
+      name: 'thumbs',
+      type: 'varchar',
+      isNullable: false,
+     },
+     {
+      name: 'rentPrice',
+      type: 'integer',
+      isNullable: false,
+     },
+     {
+      name: 'description',
+      type: 'varchar',
+      isNullable: false,
+     },
+     {
+      name: 'maxPassengers',
+      type: 'integer',
+      isNullable: false,
+     },
+     {
+      name: 'maxSpeed',
+      type: 'integer',
+      isNullable: false,
+     },
+     {
+      name: 'fuelType',
+      type: 'varchar',
+     },
+     {
+      name: 'cambioMode',
+      type: 'varchar',
+      isNullable: false,
+     },
+     {
+      name: 'horsePower',
+      type: 'integer',
+      isNullable: false,
+     },
+     {
+      name: 'igniteTime',
+      type: 'integer',
+      isNullable: false,
+     }
+    ],
+   })
+  )
+ }
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.dropTable(this.table)
-	}
-
+ public async down(queryRunner: QueryRunner): Promise<void> {
+  await queryRunner.dropTable(this.table)
+ }
 }
--- api/src/typeorm/migration/1665791744307-create-rents-table.ts
+++ api/src/typeorm/migration/1665791744307-create-rents-table.ts
@@ -1,47 +1,48 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createRentsTable1665791744307 implements MigrationInterface {
-	private table = 'rents'
+ private table = 'rents'
 
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.createTable(new Table({
-			name: this.table,
-			columns: [
-				{
-					name: 'id',
-					type: 'varchar',
-					isNullable: false,
-					isUnique: true,
-					isPrimary: true,
-					isGenerated: true,
-					generationStrategy: 'uuid'
-				},
-				{
-					name: 'carId',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'customerId',
-					type: 'varchar',
-					isNullable: false
-				},
-				{
-					name: 'startRent',
-					type: 'timestamp',
-					isNullable: false
-				},
-				{
-					name: 'endRent',
-					type: 'timestamp',
-					isNullable: false
-				}
-			]
-		}))
-	}
+ public async up(queryRunner: QueryRunner): Promise<void> {
+  await queryRunner.createTable(
+   new Table({
+    name: this.table,
+    columns: [
+     {
+      name: 'id',
+      type: 'varchar',
+      isNullable: false,
+      isUnique: true,
+      isPrimary: true,
+      isGenerated: true,
+      generationStrategy: 'uuid',
+     },
+     {
+      name: 'startRent',
+      type: 'timestamp',
+      isNullable: false,
+     },
+     {
+      name: 'endRent',
+      type: 'timestamp',
+      isNullable: false,
+     },
+		 {
+			 name: 'customerId',
+			 type: 'varchar',
+			 isNullable: false,
+		 },
+		 {
+			 name: 'carId',
+			 type: 'varchar',
+			 isNullable: false,
+		 },
+    ],
+   })
+  )
+ }
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.dropTable(this.table)
-	}
-
+ public async down(queryRunner: QueryRunner): Promise<void> {
+  await queryRunner.dropTable(this.table)
+ }
 }
