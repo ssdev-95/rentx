0d16059b5922906f0d7e5467133efc31b9d277fb
Salomao S <souza95salomao@gmail.com>
2022-10-16 17:27:03 +0000

    update: routing and cookies parsing

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,17 +1,22 @@
 import { Router } from 'express'
 import { CreateCarController } from './controllers/cars/create'
 import { RetrieveCarsController } from './controllers/cars/retrieve'
+import { CreateCustomerController } from './controllers/customers/create'
+//import { ensureAuthenticated } from './middlewares/ensure-authenticated'
+
+import { CreateRentController } from './controllers/rents/create'
 
 const router = Router()
 
-router.post(
-	'/cars/seed',
-	new CreateCarController().handle
-)
+router.post('/cars/seed', new CreateCarController().handle)
 
-router.get(
-	'/cars',
-	new RetrieveCarsController().handle
+router.get('/cars', new RetrieveCarsController().handle)
+
+router.post('/auth', new CreateCustomerController().handle)
+
+router.post(
+	'/rents/:id',
+	new CreateRentController().handle
 )
 
 export { router }
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,14 @@
-import cors from 'cors'
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 import { router } from './routes'
 
 const app = express()
 
-app.use(cors({
-	origin: '*'
-}))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
+app.use(cookieParser())
 app.use(router)
 
 export { app }
